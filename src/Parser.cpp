1c209e6102ece447079240a722d8d8f1f6404e97
Aybars Nazlica <aybarsnazlica@gmail.com>
2026-03-22 13:04:31 +0900

    feat: implement parser advance method

---
+++ src/Parser.cpp
@@ -0,0 +1,14 @@
+#include "Parser.h"
+
+#include "Token.h"
+
+namespace pinky {
+Parser::Parser(std::vector<Token> tokens, std::size_t current)
+    : Tokens(std::move(tokens)), Current(current) {}
+
+Token Parser::Advance() {
+  Token token = Tokens[Current];
+  ++Current;
+  return token;
+}
+} // namespace pinky

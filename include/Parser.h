8d3fbfc1c88d723413be88a0d97f0f41120bf9a7
Aybars Nazlica <aybarsnazlica@gmail.com>
2026-03-22 12:45:50 +0900

    feat: add parser header

---
+++ include/Parser.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <vector>
+
+#include "Model.h"
+#include "Token.h"
+
+namespace pinky {
+
+class Parser {
+public:
+  explicit Parser(std::vector<Token> tokens, std::size_t current = 0);
+  Token Advance();
+  Token Peek() const;
+  bool IsNext(TokenType expectedType) const;
+  Token Expect(TokenType expectedType);
+  Token PreviousToken() const;
+  bool Match(TokenType expectedType);
+  ExprPtr Primary();
+  ExprPtr Exponent();
+  ExprPtr Unary();
+  ExprPtr Modulo();
+  ExprPtr Multiplication();
+  ExprPtr Addition();
+  ExprPtr Comparison();
+  ExprPtr Equality();
+  ExprPtr LogicalAnd();
+  ExprPtr LogicalOr();
+  ExprPtr Expr();
+  std::unique_ptr<pinky::PrintStmt> PrintStmt(std::string end);
+  std::unique_ptr<pinky::IfStmt> IfStmt();
+  std::unique_ptr<pinky::WhileStmt> WhileStmt();
+  std::unique_ptr<pinky::ForStmt> ForStmt();
+  std::vector<ExprPtr> Args();
+  std::vector<ParamPtr> Params();
+  std::unique_ptr<pinky::FuncDecl> FuncDecl();
+  std::unique_ptr<pinky::RetStmt> RetStmt();
+  std::unique_ptr<pinky::LocalAssignment> LocalAssign();
+  StmtPtr Stmt();
+  std::unique_ptr<pinky::Stmts> Stmts();
+  std::unique_ptr<pinky::Stmts> Program();
+  std::unique_ptr<pinky::Stmts> Parse();
+
+private:
+  friend struct ParserTestAccess;
+
+  std::vector<Token> Tokens;
+  std::size_t Current = 0;
+};
+} // namespace pinky
